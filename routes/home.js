b2c5025563d58a973253b9e7171106ac34f7b366
razashkenazi <Razashkenazi178@gmail.com>
2023-06-28 21:12:57 +0300

    Update controller  including errors

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { homeView } = require('../../../OneDrive/שולחן העבודה/תואר מדעי המחשב/ד. תואר במדעי המחשב - שנה ב - סמסטר ב/א. פיתוח אפליקציות אינטרנטיות/א. פרוייקט/added - admin create product_added validation for frontend and backend_ and added token validation_v4/drone-v4/controllers/homeController')
+const { homeView } = require('../controllers/homeController')
 const router = express.Router();
 
 router.get('/', homeView)
