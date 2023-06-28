a418f24a7808a9b908af12d36d6e0a8d7d3f4d08
razashkenazi <Razashkenazi178@gmail.com>
2023-06-28 21:17:14 +0300

    Update controller  including errors

--- routes/auth/signin.js
+++ routes/auth/signin.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const { signinView, signin } = require('../../../../OneDrive/שולחן העבודה/תואר מדעי המחשב/ד. תואר במדעי המחשב - שנה ב - סמסטר ב/א. פיתוח אפליקציות אינטרנטיות/א. פרוייקט/added - admin create product_added validation for frontend and backend_ and added token validation_v4/drone-v4/controllers/auth/signinController')
-const router = express.Router();
-
+const { signinView, signin } = require('../../controllers/auth/signinController')
 router.get('/signin', signinView)
 
 router.post('/signin', signin)
