b9dd42880df6329c6c1f6886476879a4db0ac4e2
razashkenazi <Razashkenazi178@gmail.com>
2023-06-28 21:03:49 +0300

    Update controller  including errors

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,3 +1,5 @@
+const { Product } = require("../models/Product");
+
 const shopView = (req, res) => {
     res.render('shop', {
     });
@@ -8,10 +10,38 @@ const shopItems = (req, res) => {
     });
 }
 
-module.exports = {
-    shopView,
-    shopItems
+const adminShop = (req, res) => {
+    return res.render('adminShop', {
+    });
 }
 
-//update - raz
+const adminShopCreateView = (req, res) => {
+    res.render('adminShopCreate', {
+    });
+}
 
+const adminShopCreate = async (req, res) => {
+
+    const shopData = {
+        name: req.body.name,
+        detail: req.body.detail,
+        stock: req.body.stock,
+        price: req.body.price,
+        productImg: req.body.productImg,
+    }
+
+    try {
+        const product = await Product.create(shopData);
+        return res.send({ status: "success", message: "The Product data created successfully!" });
+    } catch (error) {
+        return res.send({ status: "error", message: error.message });
+    }
+}
+
+module.exports = {
+    shopView,
+    shopItems,
+    adminShop,
+    adminShopCreateView,
+    adminShopCreate,
+}
