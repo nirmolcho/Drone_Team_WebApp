4943aa3d94a5125e2c87133b0f655cad18c6c876
omer luvian <omer.luvian@gmail.com>
2023-07-03 13:02:48 +0300

    Main v7 (#16)
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	routes/auth/signout.js
    #	routes/explore.js
    #	routes/shop.js
    #	routes/support.js
    #	routes/userHome.js
    
    * update vies->partials
    
    # Conflicts:
    #	routes/auth/signout.js
    #	routes/explore.js
    #	routes/shop.js
    #	routes/support.js
    #	routes/userHome.js
    
    ---------
    
    Co-authored-by: seanhyper <sean@hypernative.io>

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-
+const mongoose = require("mongoose"); 
+const bcrypt = require("bcryptjs");
 const userSchema = mongoose.Schema(
     {
         firstName: { type: String, required: true },
