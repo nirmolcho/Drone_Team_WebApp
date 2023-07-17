10227f4ca52578cb40c5f8f0bd62704eed8d8b14
lubiomer <omer.luvian@gmail.com>
2023-07-17 21:48:30 +0300

    user controllers update

---
+++ controllers/adminDashboardController.js
@@ -0,0 +1,53 @@
+const { Order } = require("../../proj_v8/models/Order");
+const { Product } = require("../../proj_v8/models/Product");
+
+const adminDashboard = async (req, res) => {
+    try {
+        const results = await Order.aggregate([
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'user',
+                    foreignField: '_id',
+                    as: 'users',
+                }, 
+            },
+            {
+              $sort: { createdAt: -1 }
+            },
+            {
+              $group: {
+                _id: { user: "$users.firstName" },
+                count: { $sum: 1 },
+                quantity: { $sum: "$quantity" },
+                totalPrice: { $sum: "$totalPrice" },
+              }
+            }
+        ]);
+        let statistics = {};
+        let countData = [];
+        let userData = [];
+        let quantityData = [];
+        let totalPriceData = [];
+        for (let result of results) {
+            userData.push(result._id.user[0])
+            countData.push(result.count);
+            quantityData.push(result.quantity);
+            totalPriceData.push(result.totalPrice);
+        }
+        statistics.userData = userData;
+        statistics.countData = countData;
+        statistics.quantityData = quantityData;
+        statistics.totalPriceData = totalPriceData;
+        return res.render('adminDashboard', {
+            statistics: statistics,
+            status: ''
+        });
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+module.exports = {
+    adminDashboard
+}
--- controllers/auth/signupController.js
+++ controllers/auth/signupController.js
@@ -13,23 +13,23 @@ const signupView = (req, res) => {
 const signup = async (req, res) => {
     let body = req.body;
     body.role = "user";
-    
+
     // validate request
     const { error } = registerValidation(body);
-    if (error) { 
-        return res.render("signup", { status: "error", message: error.details[0].message }); 
+    if (error) {
+        return res.render("signup", { status: "error", message: error.details[0].message });
     }
 
     // check for unique user
     const emailExists = await User.findOne({ email: req.body.email });
-    if (emailExists) { 
-        return res.render("signup", { status: "error", message: "Email already exists" }); 
+    if (emailExists) {
+        return res.render("signup", { status: "error", message: "Email already exists" });
     }
-    
+
     // hash the password
     const salt = await bcrypt.genSalt(saltLength);
     const hashPassword = await bcrypt.hash(req.body.password, salt);
-    
+
     const userData = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -40,7 +40,7 @@ const signup = async (req, res) => {
 
     try {
         await User.create(userData);
-        
+
         return res.render("signin", { status: "success", message: "User successfully registered" })
     } catch (err) {
         console.log(err)
