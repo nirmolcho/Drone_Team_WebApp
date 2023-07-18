2b82af396c8e21b6e261dd56cec6834f056d6d56
lubiomer <omer.luvian@gmail.com>
2023-07-18 23:20:19 +0300

    update shop to v13

--- index.js
+++ index.js
@@ -81,6 +81,6 @@ app.delete("/file/:filename", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => console.log(`🏎  DRONE Server up and running at ${PORT}`));
