4529326c8ef83848d3dd96286c211ba007a1fe16
lubiomer <omer.luvian@gmail.com>
2023-07-19 22:26:48 +0300

    admin dash board

--- controllers/supportController.js
+++ controllers/supportController.js
@@ -1,5 +1,5 @@
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API);
+sgMail.setApiKey('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');
 
 const supportView = async (req, res) => {
     
@@ -11,9 +11,9 @@ const supportView = async (req, res) => {
 
 const contactSend = async (req, res) => {
     const msg = {
-        from: 'dronteamsend@gmail.com',
-        to: 'droneteamrecive@gmail.com',
-        subject: 'Drone Team Support Ticket',
+        from: req.body.email,
+        to: 'dronteamsend@gmail.com',
+        subject: 'Support',
         text: 'Plain text body of your email',
         html: `<p>${req.body.content}</p>`,
     };
